edc5ba830d5c1d2c7c8ff620504833266267058d
John Blackwell <jblackwell1@q-centrix.com>
2021-06-11 09:57:39 -0700

    add aliases

---
+++ work-specific.alias.zsh
@@ -0,0 +1,14 @@
+# opens qapps workspace
+alias qapps-workspace='code ~/Documents/code/workspaces/qapps.code-workspace'
+
+# runs qapps dump (accepts dump path as arg)
+alias dump='pg_restore -d currica_development --verbose --clean --no-acl --no-owner'
+
+# opens scrum/meeting zoom link
+alias scrum="work && open 'https://us02web.zoom.us/j/5394852076?pwd=bWF5VWdCQWNpUUJIZTUzK2dvZ0pKQT09'"
+
+# rails aliases
+alias rs='rails server'
+
+# starts redis in daemon
+alias redis='/Users/jblackwell/Documents/code/redis-6.0.9/src/redis-server --daemonize yes'
